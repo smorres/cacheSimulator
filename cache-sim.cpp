49bd7750c8e777a94f7d8c52b169204a25d80544
Shane Morris <smorri11@binghamton.edu>
2019-11-21 10:12:05 -0500

    incomplete

--- cache-sim.cpp
+++ cache-sim.cpp
@@ -96,10 +96,57 @@ long long setAssociative(int cacheSize, vector<instructions> instr, ofstream & o
 		
 	}
 	outfile << hit << "," << refrences << "; ";
+	cout<<"set assoc hits: " << hit<<endl;	
+	return hit;
+}
+/*
+long long fullAssociativeLRU(int cacheSize, vector<instructions> instr, ofstream & outfile, int numWays){
+	int entries = cacheSize / 32; //32 is line size, if we want this to be variable we can take lineSize as an input
+	vector<vector<info>> cache (numWays, vector<info> ((entries/numWays), {-1, 0}) );
+	long long base, cacheIndex;
+	long long hit = 0;
+	long long refrences = 0;
+	
+	for(unsigned long i = 0; i < instr.size(); i++){
+		refrences++;
+		int base = instr[i].address / 32;
+		int cacheIndex = base % (entries/numWays);
+		bool hadHit = false;
+		for(int j= 0; j < numWays; j ++){
+			if(cache[j][cacheIndex].data == (instr[i].address >> 5)/(entries/numWays)){
+				hit++;
+				hadHit = true;
+				cache[j][cacheIndex].count = i;
+			}
+		}
+		if(!hadHit){
+			//if the instr is a store instr
+			//ou dont do anthing
+			//just continue
+			int min = cache[0][cacheIndex].count;
+			int newJ=0;
+			for(int j = 0; j < numWays; j ++){
+				if(cache[j][cacheIndex].data == -1){
+					newJ = j;
+					break;
+				}
+				if(cache[j][cacheIndex].count <= min){
+					min = cache[j][cacheIndex].count;
+					newJ = j;
+					
+				}
+				
+			}			
+			cache[newJ][cacheIndex].data = (instr[i].address >> 5)/(entries/numWays);
+			cache[newJ][cacheIndex].count = i;
+		}
+		
+	}
+	outfile << hit << "," << refrences << "; ";
 	cout<<"hits: " << hit<<endl;	
 	return hit;
 }
-
+*/
 long long setAssociativeNoAlloc(int cacheSize, vector<instructions> instr, ofstream & outfile, int numWays){
 	
 	int entries = cacheSize / 32; //32 is line size, if we want this to be variable we can take lineSize as an input
@@ -148,10 +195,84 @@ long long setAssociativeNoAlloc(int cacheSize, vector<instructions> instr, ofstr
 		
 	}
 	outfile << hit << "," << refrences << "; ";
-	cout<<"hits: " << hit<<endl;	
+	cout<<"no alloc hits: " << hit<<endl;	
 	return hit;
 }
-
+long long setAssociativeWithNextLinePrefetching(int cacheSize, vector<instructions> instr, ofstream & outfile, int numWays){
+	
+	int entries = cacheSize / 32; //32 is line size, if we want this to be variable we can take lineSize as an input
+	vector<vector<info>> cache (numWays, vector<info> ((entries/numWays), {-1, 0}) );
+	int base, cacheIndex;
+	int hit = 0;
+	int refrences = 0;
+	
+	for(unsigned long i = 0; i < instr.size(); i++){
+		refrences++;
+		int base = instr[i].address / 32;
+		int cacheIndex = base % (entries/numWays);
+		bool hadHit = false;
+		for(int j= 0; j < numWays; j ++){
+			if(cache[j][cacheIndex].data == (instr[i].address >> 5)/(entries/numWays)){
+				hit++;
+				hadHit = true;
+				cache[j][cacheIndex].count = i;
+			}
+		}
+		if(!hadHit){
+			//if the instr is a store instr
+			//ou dont do anthing
+			//just continue
+			int min = cache[0][cacheIndex].count;
+			int newJ=0;
+			for(int j = 0; j < numWays; j ++){
+				if(cache[j][cacheIndex].data == -1){
+					newJ = j;
+					break;
+				}
+				if(cache[j][cacheIndex].count <= min){
+					min = cache[j][cacheIndex].count;
+					newJ = j;
+					
+				}
+				
+			}			
+			cache[newJ][cacheIndex].data = (instr[i].address >> 5)/(entries/numWays);
+			cache[newJ][cacheIndex].count = i;
+		}
+		
+		cacheIndex = (cacheIndex + 1) % entries;
+		base++;
+		bool nextLineHit = false;
+		//Check if the nextline is already in-place
+		for(int j = 0; j < numWays; j++){
+                        if(cache[j][cacheIndex].data == (instr[i].address >> 5)/(entries/numWays)){
+                                cache[i][cacheIndex].count = i;
+				nextLineHit = true;
+			}
+		}
+		if(!nextLineHit){
+			int victimIndex = -1;
+			int secondMin = cache[0][cacheIndex].count;
+			for(int j = 0; j < numWays; j++){
+				if(cache[j][cacheIndex].count <= secondMin){
+					secondMin = cache[j][cacheIndex].count;
+					victimIndex = j;
+				}
+				if(cache[j][cacheIndex].data == -1){
+					victimIndex = j;
+					break;
+				}
+			}
+                	//replacing element at victim index                     
+                	cache[victimIndex][cacheIndex].data = (instr[i].address >> 5)/(entries/numWays);
+			cache[victimIndex][cacheIndex].count = i;
+		
+		}
+	}
+	outfile << hit << "," << refrences << "; "<<endl;
+	cout<<"next line hits: " << hit<<endl;	
+	return hit;
+}
 
 int main(int argc, char *argv[]){
 	int address;
@@ -173,7 +294,14 @@ int main(int argc, char *argv[]){
 	setAssociative(16384,instr, outfile, 4);
 	setAssociative(16384,instr, outfile, 8);
 	setAssociative(16384,instr, outfile, 16);
-	setAssociativeNoAlloc(16384, instr, outfile, 512);
+	setAssociativeNoAlloc(16384, instr, outfile, 2);
+	setAssociativeNoAlloc(16384, instr, outfile, 4);
+	setAssociativeNoAlloc(16384, instr, outfile, 8);
+	setAssociativeNoAlloc(16384, instr, outfile, 16);
+	setAssociativeWithNextLinePrefetching(16384,instr, outfile, 2);
+	setAssociativeWithNextLinePrefetching(16384,instr, outfile, 4);
+	setAssociativeWithNextLinePrefetching(16384,instr, outfile, 8);
+	setAssociativeWithNextLinePrefetching(16384,instr, outfile, 16);
 	
 
 }
